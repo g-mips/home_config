6ace17032b8994d79acf757530068fe66d7c22a9
g-mips <g-mips@users.noreply.github.com>
2019-10-19 17:11:45 -0600

    Make configure.sh more user friendly and add gdb configuration to it.

--- configure.sh
+++ configure.sh
@@ -1,2 +1,46 @@
-#!/bin/bash
-cd git && ./configure.sh
+#!/bin/sh
+
+usage () {
+    echo "Usage $0 [ -hagG ]"
+    echo ""
+    echo "    -h -- print this help"
+    echo "    -a -- configure all"
+    echo "    -g -- configure git"
+    echo "    -G -- configure gdb"
+}
+
+error () {
+    usage
+    exit 1
+}
+
+GIT=false
+GDB=false
+while getopts "hagG" option
+do
+    case "${option}" in
+        h)
+            error
+            ;;
+        a)
+            GIT=true
+            GDB=true
+            ;;
+        g)
+            GIT=true
+            ;;
+        G)
+            GDB=true
+            ;;
+    esac
+done
+
+if $GIT
+then
+    cd git && ./configure.sh
+fi
+
+if $GDB
+then
+    cd gdb && ./configure.sh
+fi
