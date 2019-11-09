cf8f27f7fb8f90b7081af241f48e7a2cc61a668a
g-mips <g-mips@users.noreply.github.com>
2019-11-09 01:24:28 -0700

    Adjust gdb configure

--- gdb/configure.sh
+++ gdb/configure.sh
@@ -7,9 +7,9 @@ echo "******** GDB CONFIGURATION ********"
 
 echo "source ${ROOT}/.gdbinit" > $DEFAULT_FILE
 
-read -p "Do you want tui enabled by default (y|n)? " TUI
+read -N 1 -p "Do you want tui enabled by default (y|n)? " TUI
 
-if [ "$TUI" = "y" ] || [ "$TUI" = "Y" ]
+if $(echo $TUI) | grep -iqF y
 then
 	echo OTHER_FILES=\$\(ROOT\)/.gdb_tui > $CONFIG_FILE
     echo "source ${ROOT}/.gdb_tui" >> $DEFAULT_FILE
