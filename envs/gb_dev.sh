fe62f06a415139c1d03f4960d6ae414791a06815
g-mips <g-mips@users.noreply.github.com>
2021-10-30 13:07:07 -0600

    Add a deeper shell level to gb_dev.sh

--- envs/gb_dev.sh
+++ envs/gb_dev.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
-
 (return 0 2>/dev/null) && SOURCED=1 || SOURCED=0
 
 [ $SOURCED -eq 0 ] && echo Please source $0 && exit 1
 
-export DEVKITPRO=/opt/devkitpro
-export DEVKITARM=/opt/devkitpro/devkitARM
-export DEVKITPPC=/opt/devkitpro/devkitPPC
+printf "*************************************************\n"
+printf "Welcome to the Dev KIT Pro environment.\n"
+printf "Useful for Nintendo Wii, Gamecube, DS, GBA,\n"
+printf "Gamepark GP32 and Nintendo Switch development.\n"
+printf "*************************************************\n"
+
+env DEVKITPRO=/opt/devkitpro DEVKITARM=/opt/devkitpro/devkitARM \
+    DEVKITPPC=/opt/devkitpro/devkitPPC \
+    $(ls -o /proc/$$/exe | awk '{ print $NF }')
+
+printf "Development is finished.\n"
