e484a26f06da3c1ad3cc65a232c194a409d8f80c
g-mips <g-mips@users.noreply.github.com>
2024-06-01 10:19:50 -0600

    Cleanup tmux config and allow for X number of colors when running list_colors.sh

--- home/.local/bin/list_colors.sh
+++ home/.local/bin/list_colors.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-for i in {0..255}
+[ $# -ge 1 ] && MAX=$1 || MAX=255
+
+for i in $(eval echo {0..${MAX}})
 do
     printf "\x1b[38;5;${i}mcolor%-3i 38;5;%-7i \x1b[0m" $i $i
     if ! (( ($i + 1 ) % 8 ))
