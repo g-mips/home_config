4666ca56f0e57aaec7c42b41757d81d278392bae
g-mips <g-mips@users.noreply.github.com>
2024-06-14 01:15:09 -0600

    Update themes

--- scripts/vimrc_configure.sh
+++ scripts/vimrc_configure.sh
@@ -23,20 +23,12 @@ then
     # clone in the bundles
     rm -rf ~/.config/vim/bundle/base16-vim
     rm -rf ~/.config/vim/bundle/vim-airline
-    rm -rf ~/.config/vim/bundle/everforest
-    rm -rf ~/.config/vim/bundle/vim-markdown
 
     git clone https://github.com/chriskempson/base16-vim.git ~/.config/vim/bundle/base16-vim
     git clone https://github.com/vim-airline/vim-airline.git ~/.config/vim/bundle/vim-airline
-    git clone https://github.com/sainnhe/everforest.git ~/.config/vim/bundle/everforest
     git clone https://github.com/tpope/vim-markdown.git ~/.config/vim/bundle/vim-markdown
-
-    # Manually install certain things
-    cp -f -R ~/.config/vim/bundle/everforest/autoload/* ~/.config/vim/autoload/.
-    cp -f -R ~/.config/vim/bundle/everforest/colors/* ~/.config/vim/colors/.
-    cp -f -R ~/.config/vim/bundle/everforest/doc/* ~/.config/vim/doc/.
 
     mkdir -p ~/.config/vim/pack/themes/start
     cd ~/.config/vim/pack/themes/start
-    git clone https://github.com/dracula/vim.git dracula
+    git clone https://github.com/srcery-colors/srcery-vim.git
 fi
