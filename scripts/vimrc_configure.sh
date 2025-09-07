b792e30452a5b2025f7c482c481b4130a26074e1
g-mips <g-mips@users.noreply.github.com>
2025-09-07 17:19:45 -0600

    Update vimrc things to support new themes, new plugin system, and vimwiki.

--- scripts/vimrc_configure.sh
+++ scripts/vimrc_configure.sh
@@ -2,7 +2,7 @@
 set -e
 
 # make the proper directories
-mkdir -p ~/.config/vim/bundle
+#mkdir -p ~/.config/vim/bundle
 mkdir -p ~/.config/vim/autoload
 mkdir -p ~/.config/vim/doc
 mkdir -p ~/.config/vim/colors
@@ -18,21 +18,33 @@ printf "\n"
 if printf "%s" $SETUP_PACKAGES | grep -iqF y
 then
     # Setup the package manager
-    curl -LSso ~/.config/vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    #curl -LSso ~/.config/vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+    # Plugins
+    PLUGIN_BASE=~/.config/vim/pack/plugins/start
 
     # clone in the bundles
-    rm -rf ~/.config/vim/bundle/base16-vim
-    rm -rf ~/.config/vim/bundle/vim-airline
-    rm -rf ~/.config/vim/bundle/vim-markdown
+    rm -rf ~/.config/vim/bundle
+    rm -rf $PLUGIN_BASE/base16-vim
+    rm -rf $PLUGIN_BASE/vim-airline
+    rm -rf $PLUGIN_BASE/vim-markdown
+    rm -rf $PLUGIN_BASE/vimwiki
 
-    git clone https://github.com/chriskempson/base16-vim.git ~/.config/vim/bundle/base16-vim
-    git clone https://github.com/vim-airline/vim-airline.git ~/.config/vim/bundle/vim-airline
-    git clone https://github.com/tpope/vim-markdown.git ~/.config/vim/bundle/vim-markdown
+    git clone https://github.com/chriskempson/base16-vim.git $PLUGIN_BASE/base16-vim
+    git clone https://github.com/vim-airline/vim-airline.git $PLUGIN_BASE/vim-airline
+    git clone https://github.com/tpope/vim-markdown.git $PLUGIN_BASE/vim-markdown
+    git clone https://github.com/vimwiki/vimwiki.git $PLUGIN_BASE/vimwiki
 
-    mkdir -p ~/.config/vim/pack/themes/start
-    cd ~/.config/vim/pack/themes/start
+    # To generate documentation i.e. ':h vimwiki'
+    vim -c "helptags $PLUGIN_BASE/vimwiki/doc" -c quit
 
-    rm -rf ~/.config/vim/pack/themes/start/srcery-vim
+    # Themes
+    THEME_BASE=~/.config/vim/pack/themes/start
+    rm -rf $THEME_BASE
+    mkdir -p $THEME_BASE
+    cd $THEME_BASE
 
     git clone https://github.com/srcery-colors/srcery-vim.git
+    git clone https://github.com/NLKNguyen/papercolor-theme.git
+    git clone https://github.com/dracula/vim.git
 fi
